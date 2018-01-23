73acc2f4c603983e8e96494d781bf554c593248c
ios开发 <[715416878@qq.com]>
2018-01-23 11:46:00 +0800

    创建项目

---
+++ MVVMNews/MVVMNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMNews
+//
+//  Created by ios开发 on 2018/1/23.
+//  Copyright © 2018年 ios开发. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVMNews/MVVMNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVMNews
+//
+//  Created by ios开发 on 2018/1/23.
+//  Copyright © 2018年 ios开发. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
